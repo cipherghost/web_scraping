a9a1923ca8371631158e0e92829e1dcaca5c69bc
Pooja Yadav <poojayadav.1dec@gmail.com>
2020-10-01 01:25:30 +0530

    Create web-sc2.py

---
+++ web-sc2.py
@@ -0,0 +1,21 @@
+import http.cookiejar
+import urllib.request
+import requests
+import bs4
+
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+urllib.request.install_opener(opener)
+authentication_url = "https://m.facebook.com/login.php"
+
+# Enter your credentials.
+payload={
+    'email':"your_email",
+    'pass':"your_pass"
+}
+
+data = urllib.parse.urlencode(payload).encode('utf-8')
+req = urllib.request.Request(authentication_url, data)
+resp = urllib.request.urlopen(req)
+contents = resp.read()
+#print(contents)
